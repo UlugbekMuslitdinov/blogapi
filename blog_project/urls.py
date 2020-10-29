df3c6a0490abedd8a810d841322fc514ba4f984e
Ulugbek <ulugbekmuslitdinov@gmail.com>
2020-10-29 16:39:26 +0500

    initial commit

---
+++ blog_project/urls.py
@@ -0,0 +1,36 @@
+"""blog_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
+from rest_framework.schemas import get_schema_view
+from rest_framework_swagger.views import get_swagger_view
+
+
+API_TITLE = 'Blod Api'
+API_DESCRIPTION = 'A Web API for creating and editing blog posts.'
+schema_view = get_swagger_view(title=API_TITLE)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('posts.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/v1/rest-auth/', include('rest_auth.urls')),
+    path('api/v1/rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('docs/', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION)),
+    #path('schema/', schema_view),
+    path('swagger-docs/', schema_view),
+]
